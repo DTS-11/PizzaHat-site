114ebd6eb2735950e6c309fc6a4501095fd1e2b2
DTS <officialdts11@gmail.com>
2024-01-01 11:43:07 +0530

    Revert "changes needed for github pagaes"
    This reverts commit 0bffe9e9c8685ad488068d8dc423e3c390a7cac4.

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+    adapter: adapter()
   },
   preprocess: vitePreprocess()
 };
