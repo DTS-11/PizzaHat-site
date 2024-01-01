e1868090ae408befb8e7124e2461883c937bb22f
DTS <officialdts11@gmail.com>
2024-01-01 11:15:06 +0530

    github pages stuff

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+    adapter: adapter()
   },
   preprocess: vitePreprocess()
 };
