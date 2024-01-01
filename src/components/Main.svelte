3fe18b562c2af09f85b907c94c8492497557fd1a
DTS <officialdts11@gmail.com>
2024-01-01 16:46:27 +0530

    added stats section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -2,7 +2,8 @@
     import logo from '$lib/assets/bot-logo.png';
 </script>
 
-<main>
+<main class="overflow-x-hidden">
+    <!-- MAIN LANDING SECTION -->
     <section class="p-4 md:p-10 m-5 flex flex-col md:flex-row items-center md:items-start justify-center md:justify-start my-20 md:my-32">
         <div class="order-2 md:order-1 text-center md:text-left">
             <h1 class="font-bold text-4xl md:text-6xl m-5 md:m-10">PizzaHat</h1>
@@ -22,4 +23,28 @@
 
         <img src={logo} alt="logo" draggable="false" class="animate-bounce m-5 w-24 h-24 md:w-60 md:h-60 md:ml-0 md:mr-10 rounded-full order-1 md:order-2">
     </section>
+
+    <!-- BOT STATS SECTION -->
+    <section class="p-4 md:p-10 m-5">
+        <div class="mt-5 md:mt-7">
+            <h1 class="font-bold text-4xl md:text-6xl m-5 md:m-10 pb-5 md:pb-10" data-aos="fade-up">Stats</h1>
+            <div class="m-5 md:m-10 text-center">
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-14 mx-auto">
+                    <div class="p-4 md:p-6 bg-zinc-950 border border-indigo-950 rounded-lg" data-aos="fade-right">
+                        <h5 class="mb-2 text-lg md:text-2xl font-bold tracking-tight">Servers</h5>
+                        <p class="font-normal text-sm md:text-base">150+</p>
+                    </div>
+                    <div class="p-4 md:p-6 bg-zinc-950 border border-indigo-950 rounded-lg" data-aos="fade-up">
+                        <h5 class="mb-2 text-lg md:text-2xl font-bold tracking-tight">Users</h5>
+                        <p class="font-normal text-sm md:text-base">170,000+</p>
+                    </div>
+                    <div class="p-4 md:p-6 bg-zinc-950 border border-indigo-950 rounded-lg" data-aos="fade-left">
+                        <h5 class="mb-2 text-lg md:text-2xl font-bold tracking-tight">Commands</h5>
+                        <p class="font-normal text-sm md:text-base">100+</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 </main>
